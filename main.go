d065c70c79a89c9ce6c5b30ba04cdb4281701ae0
hilotter <hilotter3@gmail.com>
2020-03-03 21:28:37 +0900

    fmt

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-
 	"net/http"
+	"os"
 
 	"gopkg.in/go-playground/webhooks.v5/github"
-
-	"os"
 )
 
 const (
@@ -18,18 +16,19 @@ func main() {
 	hook, _ := github.New(github.Options.Secret(os.Getenv("WEBHOOK_SECRET")))
 
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		payload, err := hook.Parse(r, github.PushEvent)
 		if err != nil {
 			if err == github.ErrEventNotFound {
-				// ok event wasn;t one of the ones asked to be parsed
+				fmt.Printf("%+v", err)
 			}
 		}
 		switch payload.(type) {
-
 		case github.PushPayload:
 			push := payload.(github.PushPayload)
 			// Do whatever you want from here...
 			fmt.Printf("%+v", push)
+			fmt.Print(push.Ref)
+			fmt.Print(push.BaseRef)
 		}
 	})
 	http.ListenAndServe(":3000", nil)
